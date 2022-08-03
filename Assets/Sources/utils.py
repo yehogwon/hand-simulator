dd5c5bddbc46d1d651eb956617db203967a54397
Yeho Gwon <devstita@gmail.com>
2022-08-04 02:36:14 +0900

    editted utils.py so that log() can take multiple parameters

--- Assets/Sources/utils.py
+++ Assets/Sources/utils.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def log(s: str): 
-    if s is None:
-        return
-    print ('\033[1m' + '\033[93m' + s + '\033[0m')
+def log(*s: str): 
+    for line in s: 
+        print('\033[1m' + '\033[93m' + line + '\033[0m', end=' ')
+    print()
 
 def print_as_table(d: dict): 
     if d is None: 
@@ -12,6 +12,7 @@ def print_as_table(d: dict):
     print(df)
 
 if __name__ == '__main__': 
-    log('Run on utils.py')
+    log('Put a single parameter in the function log(). ')
+    log('Run on utils.py', 'This string is separated by a comma. If this statement works without any error, it means the function log() can take multiple string parameters. ')
     print(b'nana')
     print(bytes('nana', 'utf-8'))
